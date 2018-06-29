db5e626557ed6ab8e51f798c030c4c47c526f7a9
Abhishek Kathpal <akathpal@umd.edu>
2018-06-29 15:29:17 -0400

    writeup_updated

--- kuka_arm/scripts/IK_server.py
+++ kuka_arm/scripts/IK_server.py
@@ -45,21 +45,19 @@ def Tf_mat(alpha,a,d,q):
 			 	 [                 0,                 0,           0,             1]])
 	return Tf
 
-def kinematics():
+def rotation_wc():
+	#Computing R0_3
 
 	# Create individual transformation matrices
 	T0_1 = Tf_mat(alpha0,a0,d1,q1).subs(s)
 	T1_2 = Tf_mat(alpha1,a1,d2,q2).subs(s)
 	T2_3 = Tf_mat(alpha2,a2,d3,q3).subs(s)
-	#T3_4 = Tf_mat(alpha3,a3,d4,q4).subs(s)
-	#T4_5 = Tf_mat(alpha4,a4,d5,q5).subs(s)
-	#T5_6 = Tf_mat(alpha5,a5,d6,q6).subs(s)
-	#T6_G = Tf_mat(alpha6,a6,d7,q7).subs(s)
+	
 	R0_3 = T0_1[0:3,0:3] * T1_2[0:3,0:3] * T2_3[0:3,0:3]
 	
 	return R0_3
 
-def fk(theta):
+def fwd_kinematics(theta):
 
 	# Create individual transformation matrices
 	#T0_1 = Tf_mat(alpha0,a0,d1,q1).subs(s)
@@ -69,7 +67,6 @@ def fk(theta):
 	#T4_5 = Tf_mat(alpha4,a4,d5,q5).subs(s)
 	#T5_6 = Tf_mat(alpha5,a5,d6,q6).subs(s)
 	#T6_G = Tf_mat(alpha6,a6,d7,q7).subs(s)
-	
 	#T0_G = T0_1*T1_2*T2_3*T3_4*T4_5*T5_6*T6_G
 
 	T0_G = Matrix([[(((sin(q2)*cos(q1)*cos(q3) + sin(q3)*cos(q1)*cos(q2))*cos(q4) + sin(q1)*sin(q4))*cos(q5) + (-sin(q2)*sin(q3)*cos(q1) + cos(q1)*cos(q2)*cos(q3))*sin(q5))*cos(q6) - ((sin(q2)*cos(q1)*cos(q3) + sin(q3)*cos(q1)*cos(q2))*sin(q4) - sin(q1)*cos(q4))*sin(q6), -(((sin(q2)*cos(q1)*cos(q3) + sin(q3)*cos(q1)*cos(q2))*cos(q4) + sin(q1)*sin(q4))*cos(q5) + (-sin(q2)*sin(q3)*cos(q1) + cos(q1)*cos(q2)*cos(q3))*sin(q5))*sin(q6) - ((sin(q2)*cos(q1)*cos(q3) + sin(q3)*cos(q1)*cos(q2))*sin(q4) - sin(q1)*cos(q4))*cos(q6), -((sin(q2)*cos(q1)*cos(q3) + sin(q3)*cos(q1)*cos(q2))*cos(q4) + sin(q1)*sin(q4))*sin(q5) + (-sin(q2)*sin(q3)*cos(q1) + cos(q1)*cos(q2)*cos(q3))*cos(q5), -0.303*((sin(q2)*cos(q1)*cos(q3) + sin(q3)*cos(q1)*cos(q2))*cos(q4) + sin(q1)*sin(q4))*sin(q5) + 0.303*(-sin(q2)*sin(q3)*cos(q1) + cos(q1)*cos(q2)*cos(q3))*cos(q5) - 1.5*sin(q2)*sin(q3)*cos(q1) - 0.054*sin(q2)*cos(q1)*cos(q3) + 1.25*sin(q2)*cos(q1) - 0.054*sin(q3)*cos(q1)*cos(q2) + 1.5*cos(q1)*cos(q2)*cos(q3) + 0.35*cos(q1)], [(((sin(q1)*sin(q2)*cos(q3) + sin(q1)*sin(q3)*cos(q2))*cos(q4) - sin(q4)*cos(q1))*cos(q5) + (-sin(q1)*sin(q2)*sin(q3) + sin(q1)*cos(q2)*cos(q3))*sin(q5))*cos(q6) - ((sin(q1)*sin(q2)*cos(q3) + sin(q1)*sin(q3)*cos(q2))*sin(q4) + cos(q1)*cos(q4))*sin(q6), -(((sin(q1)*sin(q2)*cos(q3) + sin(q1)*sin(q3)*cos(q2))*cos(q4) - sin(q4)*cos(q1))*cos(q5) + (-sin(q1)*sin(q2)*sin(q3) + sin(q1)*cos(q2)*cos(q3))*sin(q5))*sin(q6) - ((sin(q1)*sin(q2)*cos(q3) + sin(q1)*sin(q3)*cos(q2))*sin(q4) + cos(q1)*cos(q4))*cos(q6), -((sin(q1)*sin(q2)*cos(q3) + sin(q1)*sin(q3)*cos(q2))*cos(q4) - sin(q4)*cos(q1))*sin(q5) + (-sin(q1)*sin(q2)*sin(q3) + sin(q1)*cos(q2)*cos(q3))*cos(q5), -0.303*((sin(q1)*sin(q2)*cos(q3) + sin(q1)*sin(q3)*cos(q2))*cos(q4) - sin(q4)*cos(q1))*sin(q5) + 0.303*(-sin(q1)*sin(q2)*sin(q3) + sin(q1)*cos(q2)*cos(q3))*cos(q5) - 1.5*sin(q1)*sin(q2)*sin(q3) - 0.054*sin(q1)*sin(q2)*cos(q3) + 1.25*sin(q1)*sin(q2) - 0.054*sin(q1)*sin(q3)*cos(q2) + 1.5*sin(q1)*cos(q2)*cos(q3) + 0.35*sin(q1)], [-(-sin(q2)*sin(q3) + cos(q2)*cos(q3))*sin(q4)*sin(q6) + ((-sin(q2)*sin(q3) + cos(q2)*cos(q3))*cos(q4)*cos(q5) + (-sin(q2)*cos(q3) - sin(q3)*cos(q2))*sin(q5))*cos(q6), -(-sin(q2)*sin(q3) + cos(q2)*cos(q3))*sin(q4)*cos(q6) - ((-sin(q2)*sin(q3) + cos(q2)*cos(q3))*cos(q4)*cos(q5) + (-sin(q2)*cos(q3) - sin(q3)*cos(q2))*sin(q5))*sin(q6), -(-sin(q2)*sin(q3) + cos(q2)*cos(q3))*sin(q5)*cos(q4) + (-sin(q2)*cos(q3) - sin(q3)*cos(q2))*cos(q5), -0.303*(-sin(q2)*sin(q3) + cos(q2)*cos(q3))*sin(q5)*cos(q4) + 0.303*(-sin(q2)*cos(q3) - sin(q3)*cos(q2))*cos(q5) + 0.054*sin(q2)*sin(q3) - 1.5*sin(q2)*cos(q3) - 1.5*sin(q3)*cos(q2) - 0.054*cos(q2)*cos(q3) + 1.25*cos(q2) + 0.75], [0, 0, 0, 1]])
@@ -96,7 +93,7 @@ def inv_kinematics(pW,R0_3,Rot_G):
 
 
 	R0_3 = R0_3.evalf(subs={q1: theta1,q2: theta2, q3: theta3})
-	#print("R0_3"+str(R0_3))
+	#LU Decomposition is used for finding the inverse of R0_3
 	R3_6 = R0_3.inv("LU")*Rot_G
 	#print(R3_6)
 	#deriving euler angles from composition of rotation matrix
@@ -119,6 +116,9 @@ def handle_calculate_IK(req):
 		
 		# Initialize service response
 		joint_trajectory_list = []
+		ee_offset = []
+		ee_offset.append(0)
+
 		for x in xrange(0, len(req.poses)):
 			# IK code starts here
 			joint_trajectory_point = JointTrajectoryPoint()
@@ -151,29 +151,44 @@ def handle_calculate_IK(req):
 			Rot_G = Rot_G*Rot_corr 
 			Rot_G = Rot_G.subs({'r':roll,'p':pitch,'y':yaw})
 
+			#End-effector position
 			pG = Matrix([[px],
 						 [py],
 						 [pz]])
-			#Wrist Center
+
+			#Wrist Center Position Computation
 			pW = pG - (0.303)*Rot_G[:,2]
 
+			#Inverse Kinematics Output Joint Angles
 			theta = inv_kinematics(pW,R0_3,Rot_G)
-			your_ee = fk(theta)
-			ee_x_e = abs(your_ee[0]-px)
-			ee_y_e = abs(your_ee[1]-py)
-			ee_z_e = abs(your_ee[2]-pz)
-			ee_offset = sqrt(ee_x_e**2 + ee_y_e**2 + ee_z_e**2)
-			print ("\nEnd effector error for x position is: %04.8f" % ee_x_e)
-			print ("End effector error for y position is: %04.8f" % ee_y_e)
-			print ("End effector error for z position is: %04.8f" % ee_z_e)
-			print ("Overall end effector offset is: %04.8f units \n" % ee_offset)
 
-			# Populate response for the IK request
-			# In the next line replace theta1,theta2...,theta6 by your joint angle variables
+			# Error Computing between calculated pose from forward kinematics and input end-effctor poses
+			# your_ee = fwd_kinematics(theta)
+			# ee_x_e = abs(your_ee[0]-px)
+			# ee_y_e = abs(your_ee[1]-py)
+			# ee_z_e = abs(your_ee[2]-pz)
+			# ee_offset.append(sqrt(ee_x_e**2 + ee_y_e**2 + ee_z_e**2))
+			
+			# #Printing Error
+			# print ("\nEnd effector error for x position is: %04.8f" % ee_x_e)
+			# print ("End effector error for y position is: %04.8f" % ee_y_e)
+			# print ("End effector error for z position is: %04.8f" % ee_z_e)
+			# print ("Overall end effector offset is: %04.8f units \n" % ee_offset[x])
+
+			# #Plotting Error
+			# plt.plot([x,x+1],[ee_offset[x],ee_offset[x+1]],'or-',linewidth=2)
+			# plt.draw()
+			# plt.pause(0.5)
+			
+			
 			joint_trajectory_point.positions = np.float64(theta)
 			joint_trajectory_list.append(joint_trajectory_point)
 
 		rospy.loginfo("length of Joint Trajectory List: %s" % len(joint_trajectory_list))
+		#plt.show()
+		#plt.pause(1)
+		# plt.close()
+		
 		return CalculateIKResponse(joint_trajectory_list)
 
 
@@ -182,11 +197,16 @@ def IK_server():
 	# initialize node and declare calculate_ik service
 	rospy.init_node('IK_server')
 	
-	#R0_3 = kinematics() 
+	#R0_3 can be calculated using rotation_wc function, but to reduce running time computed R0_3 in form of theta can be used
+	#R0_3 = rotation_wc() 
 	R0_3 = Matrix([[sin(q2)*cos(q1)*cos(q3) + sin(q3)*cos(q1)*cos(q2), -sin(q2)*sin(q3)*cos(q1) + cos(q1)*cos(q2)*cos(q3), -sin(q1)], [sin(q1)*sin(q2)*cos(q3) + sin(q1)*sin(q3)*cos(q2), -sin(q1)*sin(q2)*sin(q3) + sin(q1)*cos(q2)*cos(q3), cos(q1)], [-sin(q2)*sin(q3) + cos(q2)*cos(q3), -sin(q2)*cos(q3) - sin(q3)*cos(q2), 0]])
+	
 	s = rospy.Service('calculate_ik', CalculateIK, handle_calculate_IK)
+	
 	print "Ready to receive an IK request"
 	rospy.spin()
 
 if __name__ == "__main__":
     IK_server()
+
+ 
